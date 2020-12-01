251667cfcb9cda5726e7a1642a184259b76b4283
milton-reyes <altf4616@outlook.com>
2020-12-01 12:09:51 -0500

    Initial commit

---
+++ src/main/java/com/yukaju/LeaveCommentSrvApplication.java
@@ -0,0 +1,13 @@
+package com.yukaju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveCommentSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveCommentSrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukaju/LeaveCommentSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukaju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveCommentSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
